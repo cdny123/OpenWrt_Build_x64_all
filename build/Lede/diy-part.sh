fd42bb9e4a7c60d8819194ef8c0d036baa4acbbc
cdny123 <133751542+cdny123@users.noreply.github.com>
2025-01-17 08:28:53 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -4,24 +4,9 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
-#添加homeproxy插件
-git clone https://github.com/douglarek/luci-app-homeproxy.git package/luci-app-homeproxy
-
-#添加bypass插件
-git clone -b bypass https://github.com/shidahuilang/openwrt-package.git package/luci-app-bypass
-
-
 #添加CPU使用率、实时内存使用情况
 sed -i '/<tr><td width="33%"><%:CPU usage/a <tr><td width="33%"><%:CPU usage%></td><td><%##cpu%></td></tr>' package/lean/autocore/files/x86/index.htm
 sed -i '/<tr><td width="33%"><%:CPU usage/a <tr><td width="33%"><%:Memory usage%></td><td><%##memory%></td></tr>' package/lean/autocore/files/x86/index.htm
-sed -i '5a\msgid "Compiler author"' feeds/luci/modules/luci-base/po/zh-cn/base.po
-sed -i '6a\msgstr "固件编译者"' feeds/luci/modules/luci-base/po/zh-cn/base.po
-sed -i '7a \\' feeds/luci/modules/luci-base/po/zh-cn/base.po
-
-#添加汉化
-sed -i '8a\msgid "Firmware Update"' feeds/luci/modules/luci-base/po/zh-cn/base.po
-sed -i '9a\msgstr "固件出处"' feeds/luci/modules/luci-base/po/zh-cn/base.po
-sed -i '10a \\' feeds/luci/modules/luci-base/po/zh-cn/base.po
 
 # 后台IP设置
 export Ipv4_ipaddr="192.168.6.1"             # 修改openwrt后台地址(填0为关闭)
